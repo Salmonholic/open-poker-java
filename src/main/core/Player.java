7e23b446302ffecb12b7bbf9bb9e2d03e2c4f2e6
Felck <felix.eckardt@online.de>
2016-11-04 14:38:44 +0100

    Player

--- src/main/core/Player.java
+++ src/main/core/Player.java
@@ -1,19 +1,23 @@
 package main.core;
 
 public class Player {
+	private Table table;
 	private int money;
 	private Card[] cards;
+	private boolean fold;
+	private boolean allIn;
 	
 	/**
 	 * 
 	 * @param money
 	 */
-	public Player(int money) {
+	public Player(Table table, int money) {
+		this.table = table;
 		this.money = money;
 	}
 	
-	public int addMoney(int money) {
-		return this.money += money;
+	public void addMoney(int money) {
+		setMoney(getMoney()+money);
 	}
 
 	/**
@@ -48,6 +52,39 @@ public class Player {
 		this.cards = cards;
 	}
 	
+	public boolean isFold() {
+		return fold;
+	}
+
+	public void setFold(boolean fold) {
+		this.fold = fold;
+	}
+
+	public boolean isAllIn() {
+		return allIn;
+	}
+
+	public void setAllIn(boolean allIn) {
+		this.allIn = allIn;
+	}
+
+	public void fold() {
+		setFold(true);
+	}
 	
+	public void call() {
+		
+	}
 	
+	public void check() {
+		
+	}
+	
+	public void bet(int amount) {
+		
+	}
+	
+	public void raise(int amount) {
+		
+	}
 }
