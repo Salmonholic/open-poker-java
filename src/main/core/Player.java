7753e832284080f939977358df795b29260c4e1b
Simon Meusel <simon.meusel.pc@t-online.de>
2016-11-23 15:39:00 +0100

    Add check method

--- src/main/core/Player.java
+++ src/main/core/Player.java
@@ -97,18 +97,15 @@ public class Player {
 	}
 	
 	public void check() {
-		// TODO implement
-		// Just copied from bet
-		/*if(money > amount) {
-			currentBet += amount;
-			table.addToPot(amount);
-			addMoney(-amount);
-		} else if (money == amount) {
-			allIn();
-		} else {
-			//TODO not enough money to bet (exception?)
-			// Simon Meusel: I think you should just just go all in anyway
-		}*/
+		System.out.println(allIn);
+		if (!allIn) {
+			if (table.currentBet < money + currentBet) {
+				System.out.println(table.currentBet - currentBet);
+				addMoney(currentBet - table.currentBet);
+			} else {
+				// Not Enough Money
+			}
+		}
 	}
 	
 	public void bet(int amount) {
