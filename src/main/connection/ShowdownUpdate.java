5caa8e7580a3f2c98a70b2a55afb1f5d7e97c487
Simon Meusel <simon.meusel.pc@t-online.de>
2017-02-03 12:24:59 +0100

    Add ShowdownUpdate

---
+++ src/main/connection/ShowdownUpdate.java
@@ -0,0 +1,26 @@
+package main.connection;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+import main.core.Card;
+import main.core.Table;
+
+public class ShowdownUpdate extends Update implements Serializable {
+
+	private static final long serialVersionUID = -6296928921021225285L;
+
+	TreeMap<Integer, ArrayList<Card>> playersCards;
+
+	public ShowdownUpdate(Table table, int id) {
+		super(table, id);
+		
+		playersCards = new TreeMap<>();
+		for (int playerId : table.getPlayers().keySet()) {
+			main.core.Player player = table.getPlayer(playerId);
+			playersCards.put(playerId, player.getCards());
+		}
+	}
+
+}
